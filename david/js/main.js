026f412675e759d442fe0756ac9aff86b4008e64
David Or <davidor@david-ors-macbook.local>
2012-09-29 14:31:21 -0400

    working website

--- david/js/main.js
+++ david/js/main.js
@@ -1,1 +1,13 @@
+$(window).ready(function(){
+	//alert("window is ready"); // Popups alert window
+	$(".slidernav").click(function() {
+		var sliderChange = $(this).attr("href");
+		//sliderChange = sliderChange.replace("#", ""); // Strip out the preceding hashtag
+		$(".active").fadeOut().removeClass("active"); // Only one active div
+		$(sliderChange).fadeIn().addClass("active");
+	});
+});
 
+$(window).resize(function(){
+
+});

1776025251cabc70e50605cb244e81f3e5eded18
Vache Asatryan <vache@gagarigs.com>
2012-09-29 14:18:23 -0400

    more css

--- vachi/js/main.js
+++ vachi/js/main.js
@@ -1,1 +1,14 @@
+$(window).ready(function(){
+	// alert("window is ready ");
+	$(".slidernav").click(function() {
+  		var sliderChange = $(this).attr("href");
+  		$(".active").fadeOut().removeClass("active");
+  		// sliderChange = sliderChange.replace("#", "");
+  		$(sliderChange).fadeIn().addClass("active");
+	});
+});	
 
+$(window).resize(function(){
+	// alert("we resized");
+
+});	

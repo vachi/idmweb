c068fe2e6c108b2f1ef2560ad88114edfa295191
Jannes <jannes.meyer@gmail.com>
2012-09-30 14:40:04 -0400

    Add slideshow

--- jannes/js/main.js
+++ jannes/js/main.js
@@ -1,1 +1,13 @@
+$(function() {
+	$("#sliderlinks a").click(function(e) {
+		var oldSlider = $(".slider.active");
+		var newSlider = $(e.target.hash);
 
+		oldSlider.fadeOut().removeClass("active");
+		newSlider.fadeIn().addClass("active");
+	});
+});
+
+$(window).resize(function() {
+	console.log('resize');
+});
